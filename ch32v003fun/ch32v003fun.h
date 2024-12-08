09ad80e0030bc8ddadcbfb08b12f949c1eff120d
Jianxiong Gu <jianxiong.gu@outlook.com>
2024-12-09 03:15:11 +0800

    add ch641_pwr.h V1.0

--- ch32v003fun/ch32v003fun.h
+++ ch32v003fun/ch32v003fun.h
@@ -10895,12 +10895,16 @@ typedef struct
 #define PWR_AWU_Prescaler_10240   ((uint32_t)0x0000000E)
 #define PWR_AWU_Prescaler_61440   ((uint32_t)0x0000000F)
 
-/* STOP_mode_entry */
-#define PWR_STANDBYEntry_WFI      ((uint8_t)0x01)
-#define PWR_STANDBYEntry_WFE      ((uint8_t)0x02)
-
-/* PWR_Flag */
-#define PWR_FLAG_PVDO             ((uint32_t)0x00000004)
+#elif defined(CH641)
+/* PVD_detection_level  */
+#define PWR_PVDLevel_0          ((uint32_t)0x00000000)
+#define PWR_PVDLevel_1          ((uint32_t)0x00000020)
+#define PWR_PVDLevel_2          ((uint32_t)0x00000040)
+#define PWR_PVDLevel_3          ((uint32_t)0x00000060)
+#define PWR_PVDLevel_4          ((uint32_t)0x00000080)
+#define PWR_PVDLevel_5          ((uint32_t)0x000000A0)
+#define PWR_PVDLevel_6          ((uint32_t)0x000000C0)
+#define PWR_PVDLevel_7          ((uint32_t)0x000000E0)
 
 #elif defined(CH32V10x) || defined(CH32V20x) || defined(CH32V30x)
 
@@ -10918,17 +10922,22 @@ typedef struct
 #define PWR_Regulator_ON          ((uint32_t)0x00000000)
 #define PWR_Regulator_LowPower    ((uint32_t)0x00000001)
 
+#endif
+
+/* STOP_mode_entry */
+#define PWR_STANDBYEntry_WFI      ((uint8_t)0x01)
+#define PWR_STANDBYEntry_WFE      ((uint8_t)0x02)
+
 /* STOP_mode_entry */
 #define PWR_STOPEntry_WFI         ((uint8_t)0x01)
 #define PWR_STOPEntry_WFE         ((uint8_t)0x02)
 
 /* PWR_Flag */
+#if defined(CH32V10x) || defined(CH32V20x) || defined(CH32V30x)
 #define PWR_FLAG_WU               ((uint32_t)0x00000001)
 #define PWR_FLAG_SB               ((uint32_t)0x00000002)
-#define PWR_FLAG_PVDO             ((uint32_t)0x00000004)
-
 #endif
-
+#define PWR_FLAG_PVDO             ((uint32_t)0x00000004)
 
 /* ch32v00x_rcc.h ------------------------------------------------------------*/
 
